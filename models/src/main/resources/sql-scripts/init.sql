667b9870356fc647f26cf7123a4790916ca455d7
vc4183 <vc4183@student.uni-lj.si>
2023-01-04 18:43:39 +0100

    sprememba začetnih vrednosti

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO Store (id, title) VALUES (1,'Spar'), (2, 'Mercator'), (3, 'Hofer'), (4, 'Woolworths');
 
-INSERT INTO Product(id, title, description, category, favourite) VALUES (1, 'Kava CaffeBar', 'Mleta pražena 100g', 'kava', false), (2, 'Kava Toriavit', 'Mleta pražena 100g', 'kava', false);
+INSERT INTO Product(id, title, description, category, favourite) VALUES (1, 'Kava CaffeBar', 'Mleta pražena 100g', 'kava', true), (2, 'Kava Toriavit', 'Mleta pražena 100g', 'kava', false);
 INSERT INTO Product(id, title, description, category, favourite) VALUES (3, 'Kruh srnjakov', 'Hleb 500g', 'kruh', false), (4, 'Rokometaški kruh ', 'Hleb 800g', 'kruh', false);
 INSERT INTO Product(id, title, description, category, favourite) VALUES (5, 'Divjačinska klobasa', 'Pakirano 350g', 'suhomesnato', false), (6, 'Poletna klobasa', '450g', 'suhomesnato', false);
 
